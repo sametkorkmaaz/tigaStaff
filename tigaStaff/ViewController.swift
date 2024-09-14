d48c7c55536a8013063f53b40611949910827fa7
sametkorkmaaz <smt.krkmz.2002@gmail.com>
2024-09-15 02:08:45 +0300

    Initial Commit

---
+++ tigaStaff/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tigaStaff
+//
+//  Created by Samet Korkmaz on 15.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
